0bc09d985f471140e7d1fb949ff0716a1800ed26
gv301 <60136687+gv301@users.noreply.github.com>
2022-10-20 08:44:44 +0100

    Added clue

---
+++ gameDev-GroupH/Assets/Scripts/PickUps/ClueController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClueController : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float bobSpeed;
+    public float ceiling;
+    public float floor;
+    bool moveUp;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        Bob();
+
+    }
+
+    // Clue rotation
+    void Rotate()
+    {
+        
+        transform.Rotate(new Vector3(0, 1, 0) * rotationSpeed * Time.deltaTime);
+    }
+
+    // Clue bobbing up and down
+    void Bob()
+    {
+        if (moveUp)
+        {
+            transform.Translate(new Vector3(0, 0.1f, 0) * bobSpeed * Time.deltaTime);
+            if (transform.position.y > ceiling)
+            {
+                moveUp = false;
+            }
+
+        }
+        else
+        {
+            transform.Translate(new Vector3(0, -0.1f, 0) * bobSpeed * Time.deltaTime);
+            if (transform.position.y < floor)
+            {
+                moveUp = true;
+            }
+        }
+    }
+}
