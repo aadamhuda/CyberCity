81836a2c4aa9faf273d1128ce3950e0d5633f305
Aadam Huda <a.huda@hotmail.co.uk>
2022-11-06 15:27:11 +0000

    restart button, clue count, wait times, bug fixes

--- gameDev-GroupH/Assets/Scripts/SceneShift/MenuController.cs
+++ gameDev-GroupH/Assets/Scripts/SceneShift/MenuController.cs
@@ -10,6 +10,7 @@ public class MenuController : MonoBehaviour
     {
         saveState.Clue.Clear();
         saveState.Death.Clear();
+        saveState.ClueCount = 0;
         saveState.SaveLocation((float)-115.4, 1, (float)-65.9);
     }
     public void PlayGame()
