a9812e0698087ff2231da05c34479633d9e625e9
Ibraheem717 <78417839+Ibraheem717@users.noreply.github.com>
2022-11-27 18:56:31 +0000

    EnemySpawnRework

--- gameDev-GroupH/Assets/Scripts/OutBattle/Enemy/EnemyCollider.cs
+++ gameDev-GroupH/Assets/Scripts/OutBattle/Enemy/EnemyCollider.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Linq;
 
 public class EnemyCollider : MonoBehaviour
 {
+    private EnemyOutOfCombat [] Enemies;
+    [SerializeField]
+    private GameObject enemy_object;
+
+    [SerializeField]
+    private EnemyPatrols[] plane;
+
+
     protected bool inRange = false;
     public GameObject player;
     public SaveData PosSave;
     public TextMeshProUGUI engage;
 
+
+    private void Start()
+    {
+        Debug.Log("I dnt like this keyboard : " + gameObject.layer);
+/*        EnemyPatrolInfo temp = plane[0].GetPatrols()[0];
+        GameObject obj = Instantiate(enemy_object, temp.GetFirst(), Quaternion.identity, gameObject.transform);
+        var assigner = obj.GetComponent<EnemyOutOfCombat>();
+        assigner.player = player;
+        assigner.engage = engage;
+        assigner.whatisGround = plane[0].GetLayer();
+        assigner.PatrolPoints = temp.GetPath();*/
+        Enemies = new EnemyOutOfCombat[4 * PosSave.GetDifficulty()];
+        for (int area = 0; area < 4; area++)
+        {
+            for (int i = 0; i < PosSave.GetDifficulty(); i++)
+            {
+
+                EnemyPatrolInfo temp = plane[area].GetPatrols()[i];
+                GameObject enem = Instantiate(enemy_object, temp.GetFirst(), Quaternion.identity, gameObject.transform);
+                var assigner = enem.GetComponent<EnemyOutOfCombat>();
+                assigner.player = player;
+                assigner.engage = engage;
+                assigner.whatisGround = plane[area].GetLayer();
+                assigner.PatrolPoints = temp.GetPath();
+                assigner.whatisGround = plane[area].GetLayer();
+            }
+        }
+
+
+    }
 
     public bool getInRange()
     {
