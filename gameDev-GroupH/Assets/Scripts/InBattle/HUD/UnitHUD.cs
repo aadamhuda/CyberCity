af1043ca7a56e8e74d921e7c1a2ff65711d66d69
Aadam Huda <a.huda@hotmail.co.uk>
2023-01-07 00:01:48 +0000

    unit hud update, bars work for easy mode

--- gameDev-GroupH/Assets/Scripts/InBattle/HUD/UnitHUD.cs
+++ gameDev-GroupH/Assets/Scripts/InBattle/HUD/UnitHUD.cs
@@ -14,6 +14,7 @@ public class UnitHUD : MonoBehaviour
     public void updateHUD(BattleUnit unit)
     {
         unitDisplayName.text = unit.unitName;
+        hpSlider.maxValue = unit.maxHP;
         SetHealth(unit);
     }
 
